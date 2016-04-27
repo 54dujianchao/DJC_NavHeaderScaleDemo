714853a8889ff0c6642dc203a54f52e67db98786
DujianChao <529892433@qq.com>
2016-04-27 14:12:56 +0800

    Nav放大缩小添加约束

---
+++ NavHeaderScaleDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NavHeaderScaleDemo
+//
+//  Created by 杜建超 on 16/4/27.
+//  Copyright © 2016年 杜建超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NavHeaderScaleDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  NavHeaderScaleDemo
+//
+//  Created by 杜建超 on 16/4/27.
+//  Copyright © 2016年 杜建超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
